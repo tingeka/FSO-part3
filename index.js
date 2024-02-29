56b371b9babd70b5d2ae9c0c115a4141e3ff64a2
tingeka <53122864+tingeka@users.noreply.github.com>
2024-02-29 11:38:38 -0300

    Fix some duplicated code for some reason

--- index.js
+++ index.js
@@ -26,59 +26,6 @@ let persons = [
     }
 ]
 
-app.use(express.json())
-// app.use(morgan('tiny'))
-morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
-app.use(morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      tokens.body(req, res)
-    ].join(' ')
-  }))
-const morgan = require('morgan')
-const { v4: uuidv4 } = require('uuid');
-
-let persons = [
-    { 
-      "id": '87cf550b-d42d-450e-9539-e5ba05197d31',
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 'c10151d5-7427-4b66-9b41-f09e72a5b817',
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": '632ed2dd-3158-4a9e-9f28-42043e2e2531',
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": '4e032fd3-5bbe-4a86-ae9f-fc6f8ba64f6e',
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-app.use(express.json())
-// app.use(morgan('tiny'))
-morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
-app.use(morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      tokens.body(req, res)
-    ].join(' ')
-  }))
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
